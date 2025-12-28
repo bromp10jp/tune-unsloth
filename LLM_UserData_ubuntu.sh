ddd6ff0866d7724ff2cf59a5f37a21e5b095d065
bro <32559692+bromp10jp@users.noreply.github.com>
2025-12-28 20:24:02 +0900

    Refactor LLM_UserData_ubuntu.sh script

--- LLM_UserData_ubuntu.sh
+++ LLM_UserData_ubuntu.sh
@@ -39,8 +39,8 @@ cd $HOME
 python3 -m venv venv
 source venv/bin/activate
 pip install -U pip
-#git clone http://34.168.41.36/git/shirai/fine-tune.git
-#cd fine-tune
+git clone https://github.com/bromp10jp/tune-unsloth.git
+#cd tune-unsloth
 #pip install -U -r requirements.txt
 #python unsloth_learn.py
 #python unsloth_merge.py
@@ -57,36 +57,14 @@ pip install -r requirements.txt
 sudo apt install -y git cmake build-essential ninja-build libcurl4-openssl-dev
 cmake -B build -DLLAMA_BUILD_SERVER=OFF -DLLAMA_BUILD_EXAMPLES=OFF -DLLAMA_BUILD_TESTS=OFF
 cmake --build build -j4 --target llama-quantize
-#cd ~/fine-tune
+#cd ~/tune-unsloth
 #python ../llama.cpp/convert_hf_to_gguf.py ./merged_model_full --outfile model-f16.gguf --outtype f16
 #~/llama.cpp/build/bin/llama-quantize ./model-f16.gguf ./model-q4_k_m.gguf Q4_K_M
 #aws s3 cp ./model-q4_k_m.gguf s3://oregon-up/
-#sudo systemctl start ollama
-#sleep 2
-#ollama create model-q4 -f Modelfile.txt
 deactivate
 
 EOF
 su - -c "(cp /tmp/commandfile ~/.;bash ~/commandfile)" ubuntu
 
-# LMStudio X-Window
-LMStudio() {
-    apt install -y xfce4 xfce4-goodies xrdp
-    systemctl enable xrdp
-    systemctl start xrdp
-    useradd -m -s /bin/bash brompton
-    echo "brompton:Dnpmetro1177#" | chpasswd
-
-    cat > /tmp/commandfile2 <<EOF
-cd $HOME
-echo "startxfce4" > ~/.xsession
-wget https://installers.lmstudio.ai/linux/x64/0.3.23-3/LM-Studio-0.3.23-3-x64.AppImage
-chmod 755 LM-Studio-0.3.23-3-x64.AppImage
-echo "./LM-Studio-0.3.23-3-x64.AppImage --no-sandbox" > start_lmstudio
-chmod 755 start_lmstudio
-EOF
-    su - -c "(cp /tmp/commandfile2 ~/.;bash ~/commandfile2)" brompton
-}
-
-# EndCheck
 echo "UserData Completed."
+
